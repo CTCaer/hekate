ded959c44949ad0b5c8d18b364a4450c12230927
CTCaer <ctcaer@gmail.com>
2023-04-06 17:38:36 +0300

    Bump hekate to v6.0.3 and Nyx to v1.5.3

--- bootloader/main.c
+++ bootloader/main.c
@@ -1475,7 +1475,7 @@ ment_t ment_top[] = {
 	MDEF_END()
 };
 
-menu_t menu_top = { ment_top, "hekate v6.0.2", 0, 0 };
+menu_t menu_top = { ment_top, "hekate v6.0.3", 0, 0 };
 
 extern void pivot_stack(u32 stack_top);
 
