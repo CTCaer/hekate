80b3651d0a17a3fc7fa7a5b7e2f7e9de0d476948
CTCaer <ctcaer@gmail.com>
2024-06-11 09:08:33 +0300

    nyx: correct touch fw id display

--- nyx/nyx_gui/frontend/gui_info.c
+++ nyx/nyx_gui/frontend/gui_info.c
@@ -918,7 +918,7 @@ static lv_res_t _create_window_fuses_info_status(lv_obj_t *btn)
 			strcat(txt_buf, "#FFDD00 Error!#");
 
 		s_printf(txt_buf + strlen(txt_buf), "\n#FF8000 ID:# %02X.%02X.%02X.%02X (",
-			(touch_fw.fw_id >> 24) & 0xFF, (touch_fw.fw_id >> 24) & 0xFF, (touch_fw.fw_id >> 24) & 0xFF, touch_fw.fw_id & 0xFF);
+			(touch_fw.fw_id >> 24) & 0xFF, (touch_fw.fw_id >> 16) & 0xFF, (touch_fw.fw_id >> 8) & 0xFF, touch_fw.fw_id & 0xFF);
 
 		// Check panel pair info.
 		switch (touch_fw.fw_id)
