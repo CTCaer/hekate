36d415461ef32737c6e6fc41763fe2d2e1941721
CTCaer <ctcaer@gmail.com>
2025-01-24 16:17:05 +0200

    fatfs: set default year to 2025

--- bootloader/libs/fatfs/ffconf.h
+++ bootloader/libs/fatfs/ffconf.h
@@ -256,7 +256,7 @@
 #define FF_FS_NORTC		1
 #define FF_NORTC_MON	1
 #define FF_NORTC_MDAY	1
-#define FF_NORTC_YEAR	2024
+#define FF_NORTC_YEAR	2025
 /* The option FF_FS_NORTC switches timestamp function. If the system does not have
 /  any RTC function or valid timestamp is not needed, set FF_FS_NORTC = 1 to disable
 /  the timestamp function. Every object modified by FatFs will have a fixed timestamp
