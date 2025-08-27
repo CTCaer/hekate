fee75711351cff1f8d3f65f5b87766f217946435
CTCaer <ctcaer@gmail.com>
2025-08-27 15:02:27 +0300

    bdk: mc: carveouts are not set by cfg so fix them
    For HOS <= 3.0.2 the carveouts are set by bootloader and sdram config actually does not set them.
    So add which need different value from reset and also make sure that data is flushed for WPR config.

--- bdk/mem/mc.h
+++ bdk/mem/mc.h
@@ -21,8 +21,7 @@
 #include <mem/mc_t210.h>
 
 void mc_config_tsec_carveout(u32 bom, u32 size1mb, bool lock);
-void mc_config_carveout();
-void mc_config_carveout_finalize();
+void mc_config_carveout_hos();
 void mc_enable_ahb_redirect();
 void mc_disable_ahb_redirect();
 bool mc_client_has_access(void *address);
