813346f796a63d8095c24add5f969e92c742baf1
CTCaer <ctcaer@gmail.com>
2025-11-11 13:52:00 +0200

    bdk: bpmp: add binX clock defines

--- bdk/soc/bpmp.h
+++ bdk/soc/bpmp.h
@@ -1,7 +1,7 @@
 /*
  * BPMP-Lite Cache/MMU and Frequency driver for Tegra X1
  *
- * Copyright (c) 2019-2024 CTCaer
+ * Copyright (c) 2019-2025 CTCaer
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -64,9 +64,13 @@ typedef enum
 	BPMP_STATE_FIQ     = BIT(3),
 } bpmp_state_t;
 
-#define BPMP_CLK_LOWEST_BOOST  BPMP_CLK_HIGH2_BOOST
-#define BPMP_CLK_LOWER_BOOST   BPMP_CLK_SUPER_BOOST
-#define BPMP_CLK_DEFAULT_BOOST BPMP_CLK_HYPER_BOOST
+#define BPMP_CLK_BIN0_BOOST    BPMP_CLK_HYPER_BOOST
+#define BPMP_CLK_BIN1_BOOST    BPMP_CLK_SUPER_BOOST
+#define BPMP_CLK_BIN2_BOOST    BPMP_CLK_HIGH2_BOOST
+#define BPMP_CLK_BIN3_BOOST    BPMP_CLK_HIGH_BOOST
+
+#define BPMP_CLK_LOWER_BOOST   BPMP_CLK_BIN1_BOOST
+#define BPMP_CLK_DEFAULT_BOOST BPMP_CLK_BIN0_BOOST
 
 void bpmp_mmu_maintenance(u32 op, bool force);
 void bpmp_mmu_set_entry(int idx, const bpmp_mmu_entry_t *entry, bool apply);
