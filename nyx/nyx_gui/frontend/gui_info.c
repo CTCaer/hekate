43dec3021f785f45c48674761e5b86798c2f914c
CTCaer <ctcaer@gmail.com>
2025-11-16 21:54:03 +0200

    nyx: info: add hos version for 22 fuses burnt

--- nyx/nyx_gui/frontend/gui_info.c
+++ nyx/nyx_gui/frontend/gui_info.c
@@ -614,7 +614,10 @@ static lv_res_t _create_window_hw_info_status(lv_obj_t *btn)
 		strcpy(fuses_hos_version, "19.0.0 - 19.0.1");
 		break;
 	case 21:
-		strcpy(fuses_hos_version, "20.0.0+");
+		strcpy(fuses_hos_version, "20.0.0 - 20.5.0");
+		break;
+	case 22:
+		strcpy(fuses_hos_version, "21.0.0+");
 		break;
 	case 255:
 		strcpy(fuses_hos_version, "#FFD000 Overburnt#");
